025eee29eb267646d8bee1ac1c14805460496b90
mavenhater <sebsti222@gmail.com>
2025-05-31 23:48:42 +0200

    Player Movement

--- gae-ca2/scripts/player/player.gd
+++ gae-ca2/scripts/player/player.gd
@@ -1,11 +1,21 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed: float = 200.0
+@export var stop_distance: float = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - global_position
+	var distance = direction.length()
+
+	if distance > stop_distance:
+		velocity = direction.normalized() * speed
+	else:
+		velocity = Vector2.ZERO  # Stoppt die Bewegung
+
+	move_and_slide()
