2efbefa0f7c175ae524cb1feb8e6f21484a64b07
Lukas <lukaswirch@gmail.com>
2025-06-03 15:25:13 +0200

    Added global.gd

--- gae-ca2/scripts/game/game.gd
+++ gae-ca2/scripts/game/game.gd
@@ -3,9 +3,11 @@ extends Node2D
 @onready var health = $Health
 @onready var game_timer = $Gametimer
 @onready var enemy_manager = $EnemyManager
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Global.player= player
 	game_timer.start()
 	health.max_health = 100
 	health.current_health = 100
