e0b353ff462e4a8cb8bf0c925b9f34d53bbcb027
mavenhater <sebsti222@gmail.com>
2025-06-09 15:14:20 +0200

    deathscreen Button

--- gae-ca2/scripts/deathscreen/texture_button.gd
+++ gae-ca2/scripts/deathscreen/texture_button.gd
@@ -1,16 +1,18 @@
 extends TextureButton
-
+@onready var deathscreen_button: AnimatedSprite2D = $AnimatedSprite2D
+var start_menu_after_animation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	deathscreen_button.visible = false
+	deathscreen_button.stop()
+	start_menu_after_animation = false
 
 func _on_pressed() -> void:
-	pass
-	#get_tree().change_scene_to_file("res://scenes/deathscreen/deathscreen.tscn")
+	deathscreen_button.visible = true
+	deathscreen_button.play("click")
+	start_menu_after_animation = true
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if start_menu_after_animation:
+		get_tree().change_scene_to_file("res://scenes/mainMenu/Main_Menu.tscn")
