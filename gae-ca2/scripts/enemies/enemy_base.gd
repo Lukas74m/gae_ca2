ceafa49c3149ec96f28f901c91f94308fe117667
mavenhater <sebsti222@gmail.com>
2025-06-10 12:31:35 +0200

    Refactoring: Player states and Enemy states

--- gae-ca2/scripts/enemies/enemy_base.gd
+++ gae-ca2/scripts/enemies/enemy_base.gd
@@ -4,6 +4,9 @@ class_name Enemy
 signal died
 signal attacked(player)
 
+enum EnemyState { WALK, ATTACK, DEAD }
+var current_state: EnemyState = EnemyState.WALK
+
 @export var enemy_resource: EnemyResource
 @onready var enemy_visual = $Sprite2D
 @onready var health := $Health
@@ -14,6 +17,7 @@ var attack_range: float
 var attack_damage: float
 var attack_cooldown: float
 
+var attack_cooldown_timer: float = 0.0
 
 func _ready():
 	var texture = load(enemy_resource.texture)
@@ -28,28 +32,56 @@ func _ready():
 	health.set_healthbar_position(global_position + Vector2(-15,50))
 	health.died.connect(die)
 	
-
-# attack_cooldown is missing for attack
 func _physics_process(delta):
-	move_towards_player(delta)
-
-func move_towards_player(delta: float):
+	
+	if attack_cooldown_timer > 0.0:
+		attack_cooldown_timer -= delta
 	if Global.player == null:
 		push_error("Global.player is null")
+		velocity = Vector2.ZERO
+		move_and_slide()
 		return
+		
 	var distance_to_player = global_position.distance_to(Global.player.global_position)
+	match current_state:
+		EnemyState.WALK:
+			move_towards_player(delta, distance_to_player)
+		EnemyState.ATTACK:
+			attack_player(delta, distance_to_player)
+		EnemyState.DEAD:
+			velocity = Vector2.ZERO
+	move_and_slide()
+			
+func move_towards_player(delta: float, distance_to_player: float):
+	# If in attack range and cooldown 0
+	if distance_to_player <= attack_range and attack_cooldown_timer <= 0.0:
+		change_state(EnemyState.ATTACK)
+		return
 	# Only move if not in attack range
 	if distance_to_player > attack_range:
 		var direction = (Global.player.global_position - global_position).normalized()
 		velocity = direction * movement_speed
-		move_and_slide()
 	else:
-		# Stop moving when in attack range
+		# Stop moving when in attack range but cooldown >= 0
 		velocity = Vector2.ZERO
-		# Try to attack if possible
-		if can_attack():
-			attack()
 
+func attack_player(delta:float, distance_to_player: float):
+	velocity = Vector2.ZERO
+	#if distance_to_player > enemy_resource.attack_range:
+	#	change_state(EnemyState.WALK)
+
+func change_state(new_state: EnemyState):
+	if current_state == new_state:
+		return
+		
+	current_state = new_state
+	
+	match new_state:
+		EnemyState.ATTACK:
+			attack()
+		EnemyState.DEAD:
+			die()
+		
 func take_damage(amount: int):
 	health.update_health(-amount)
 
@@ -58,8 +90,10 @@ func die():
 	queue_free()
 	
 
-func can_attack() -> bool:
-	return global_position.distance_to(Global.player.global_position) <= attack_range
+#func can_attack() -> bool:
+#	return global_position.distance_to(Global.player.global_position) <= attack_range
 
 func attack():
+	attack_cooldown_timer = attack_cooldown
 	Global.player.take_damage(attack_damage)
+	change_state(EnemyState.WALK)
