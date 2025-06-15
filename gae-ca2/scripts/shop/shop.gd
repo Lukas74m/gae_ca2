9eada4d3687108fcd87cf4217268e7a0373e9a7c
ConexD <2021987@stud.hs-mannheim.de>
2025-06-15 17:58:43 +0200

    Shop und Upgrades

---
+++ gae-ca2/scripts/shop/shop.gd
@@ -0,0 +1,54 @@
+extends CanvasLayer
+
+@onready var btn_option1: TextureButton = $Panel/HBoxContainer/Option1Button
+@onready var btn_option2: TextureButton = $Panel/HBoxContainer/Option2Button
+@onready var panel: Panel = $Panel
+@onready var label1: Label = $Panel/HBoxContainer/Option1Button/Label
+@onready var label2: Label = $Panel/HBoxContainer/Option2Button/Label
+
+var player
+
+
+var possible_upgrades = [
+	{"stat": "attack_damage", "amount": 15, "label": "+15 Angriffsschaden"},
+	{"stat": "max_health", "amount": 25, "label": "+25 Max. Leben"},
+	{"stat": "krit_rate", "amount": 15, "label": "+15 Krit-Wahrscheinlichkeit"},
+	{"stat": "krit_damage", "amount": 25, "label": "+25 Kritischer Schaden"}
+]
+
+var chosen_upgrades = []  
+
+func show_shop(p_player):
+	player = p_player
+	chosen_upgrades.clear()
+
+	# Two differend upgrade-types
+	var upgrade_pool = possible_upgrades.duplicate()
+	upgrade_pool.shuffle()
+	chosen_upgrades.append(upgrade_pool[0])
+	chosen_upgrades.append(upgrade_pool[1])
+
+	label1.text = chosen_upgrades[0]["label"]
+	label2.text = chosen_upgrades[1]["label"]
+
+	panel.show()
+	#get_tree().paused = true
+
+
+func _on_option_1_button_pressed() -> void:
+	_apply_upgrade(0)
+
+
+func _on_option_2_button_pressed() -> void:
+	_apply_upgrade(1)
+	
+func _apply_upgrade(index):
+	if player and index < chosen_upgrades.size():
+		var upgrade = chosen_upgrades[index]
+		player.get_node("PlayerStats").base_stats[upgrade["stat"]] += upgrade["amount"]
+		print(player.get_node("PlayerStats").base_stats[upgrade["stat"]])
+		_close_shop()
+		
+func _close_shop():
+	panel.hide()
+	get_tree().paused = false
