48d6d1bdccb8b01a83f8a7ddcf9297f2cf182ae4
Shetar <leon.zeiger10@gmail.com>
2025-06-16 18:29:39 +0200

    created a boss

---
+++ gae-ca2/scripts/manager/boss_ressource.gd
@@ -0,0 +1,12 @@
+extends EnemyResource
+class_name BossResource
+
+# Additional boss-specific properties
+@export var ranged_attack_range: float
+@export var ranged_attack_damage: float
+@export var ranged_attack_cooldown: float
+@export var projectile_speed: float
+@export var melee_attack_range: float
+@export var melee_attack_damage: float
+@export var melee_attack_cooldown: float
+@export var projectile_scene_path: String
